59c5e40e7c69981b850a33d3c3fbb340292a8cf9
Lucas Rijllart <lucas.rijllart@huvrdata.com>
2025-03-28 09:04:05 -0500

    add library and config file

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+// https://rollupjs.org/troubleshooting/#warning-treating-module-as-external-dependency
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    dir: 'output',
+    format: 'cjs'
+  },
+  plugins: [nodeResolve()]
+};
