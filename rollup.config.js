ff421dba454cc5e4c0168a976f03245658c06d88
Lucas Rijllart <lucas.rijllart@huvrdata.com>
2025-04-03 13:04:20 -0500

    rollup with dependencies included

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,17 @@
-// https://rollupjs.org/troubleshooting/#warning-treating-module-as-external-dependency
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+/** rollup configuration file
+ * This file is used to bundle the JavaScript files using Rollup
+ * We are using plugin-node-resolve and plugin-commonjs to resolve and bundle the dependencies
+ * Into a single distributable file (evaluate.js) in the dist folder
+ */
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js", // Entry point of the application
   output: {
-    dir: 'output',
-    format: 'cjs'
+    file: "dist/evaluate.js", // Output file
+    format: "umd", // Universal Module Definition format
+    name: "evaluate", // Name of the global variable
   },
-  plugins: [nodeResolve()]
+  plugins: [commonjs(), nodeResolve()], // Plugins to resolve and bundle dependencies
 };
