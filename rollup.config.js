4aced2df23f5722df351340483703997000326ef
Scott White <sww314@gmail.com>
2026-02-26 08:44:13 -0600

    infra: modernize CI/CD and package configuration (#29)

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,27 @@
 /** rollup configuration file
  * This file is used to bundle the JavaScript files using Rollup
  * We are using plugin-node-resolve and plugin-commonjs to resolve and bundle the dependencies
- * Into a single distributable file (evaluate.js) in the dist folder
+ * Into distributable files in the dist folder
  */
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
   input: "src/index.js", // Entry point of the application
-  output: {
-    file: "dist/evaluate.js", // Output file
-    format: "umd", // Universal Module Definition format
-    name: "evaluate", // Name of the global variable
-  },
+  output: [
+    {
+      file: "dist/evaluate.mjs", // ESM output
+      format: "es",
+    },
+    {
+      file: "dist/evaluate.cjs", // CommonJS output
+      format: "cjs",
+    },
+    {
+      file: "dist/evaluate.umd.js", // UMD output (browsers)
+      format: "umd",
+      name: "evaluate",
+    },
+  ],
   plugins: [commonjs(), nodeResolve()], // Plugins to resolve and bundle dependencies
 };
